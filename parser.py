a6952765b49a5be36d783ea8fc4215b6515bb6e6
hitnik <hitnik@gmail.com>
2020-08-06 08:57:09 +0300

    function for create urls with params as month and year

--- parser.py
+++ parser.py
@@ -1,6 +1,7 @@
 import requests
 from requests.models import PreparedRequest
 import datetime
+import dateutil.relativedelta
 
 DOMAIN = 'http://www.pogoda.by/news/index.php'
 
@@ -9,22 +10,20 @@ def get_url(url):
     if response.status_code == 200:
         return response.text
     else:
-        raise HTTPError(response.status_code)
+        raise requests.HTTPError(response.status_code)
 
 
 def create_pogoda_news_urls_list(date_from, years_deep):
     months_deep = years_deep*12;
     urls_list = []
     for month in range(months_deep+1):
-        date = date_from - datetime.timedelta(months=month)
+        date = date_from - dateutil.relativedelta.relativedelta(months=month)
         month = date.month
         year = date.year
         params = {'month': month, 'year':year}
         req = PreparedRequest()
         req.prepare_url(DOMAIN, params)
-        print(req.url)
-
-
+        urls_list.append(req.url)
 
 if __name__ == '__main__':
     create_pogoda_news_urls_list(datetime.datetime.now(), 1)

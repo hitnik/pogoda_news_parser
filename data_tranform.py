b9c48a32b4b3bb9ae7b0f51b337f7175d21c3252
hitnik <hitnik@gmail.com>
2020-09-13 00:09:46 +0300

    chack days values by hand

--- data_tranform.py
+++ data_tranform.py
@@ -87,44 +87,110 @@ from utils import MONTHS_GEN
 #             print(d)
 #             writer.writerow(v for k, v in d.items())
 
+#
+# bag = set()
+# for i in range(1,32):
+#     bag.add(str(i))
+#     if i < 31:
+#         bag.add(str(i)+'-'+str(i+1))
+#
+# with open('weather_data_3.csv', encoding='utf-8', newline='') as csvfile:
+#     reader = csv.reader(csvfile)
+#     row_names = next(reader)
+#     row_names.remove('day_start')
+#     row_names.remove('day_end')
+#     row_names.remove('text')
+#     row_names.remove('month')
+#
+# with open('weather_data_3.csv', encoding='utf-8', newline='') as csvfile:
+#     bag_dict = {}
+#     dict_list = []
+#     reader_2 = csv.DictReader(csvfile)
+#     for row in reader_2:
+#         print(row)
+#         row_dict = {}
+#         for item in bag:
+#             bag_dict[item] = 0
+#         for name in row_names:
+#             for k,v in bag_dict.items():
+#                 for mon in MONTHS_GEN.values():
+#                     pattern = k+mon
+#                     if pattern == name:
+#                         bag_dict[k] += int(row[name])
+#         if 0 < int(row['day_start']) <= 31:
+#             row_dict['text'] = row['text']
+#             row_dict.update(bag_dict)
+#             row_dict['day_start'] = row['day_start']
+#             row_dict['day_end'] = row['day_end']
+#             dict_list.append(row_dict)
+#     with open('weather_data_days.csv', 'w', encoding='utf-8', newline='') as file:
+#         writer = csv.writer(file)
+#         writer.writerow(k for k, v in dict_list[0].items())
+#         for d in dict_list:
+#             print(d)
+#             writer.writerow(v for k, v in d.items())
 
-bag = set()
-for i in range(1,32):
-    bag.add(str(i))
-    if i < 31:
-        bag.add(str(i)+'-'+str(i+1))
+import pprint
 
-with open('weather_data_3.csv', encoding='utf-8', newline='') as csvfile:
-    reader = csv.reader(csvfile)
+pp = pprint.PrettyPrinter(width=100)
+
+with open('weather_data_days.csv', encoding='utf-8', newline='') as file:
+    reader = csv.reader(file)
     row_names = next(reader)
     row_names.remove('day_start')
     row_names.remove('day_end')
     row_names.remove('text')
-    row_names.remove('month')
 
-with open('weather_data_3.csv', encoding='utf-8', newline='') as csvfile:
-    bag_dict = {}
-    dict_list = []
+    print(row_names)
+
+dict_list = []
+
+with open('weather_data_days.csv', encoding='utf-8', newline='') as csvfile:
     reader_2 = csv.DictReader(csvfile)
+    cont = True
     for row in reader_2:
-        print(row)
+        day_start = row['day_start']
+        day_end = row['day_end']
         row_dict = {}
-        for item in bag:
-            bag_dict[item] = 0
-        for name in row_names:
-            for k,v in bag_dict.items():
-                pattern = re.compile('^'+k)
-                if re.match(pattern, name):
-                    bag_dict[k] += int(row[name])
-        if 0 < int(row['day_start']) <= 31:
-            row_dict['text'] = row['text']
-            row_dict.update(bag_dict)
-            row_dict['day_start'] = row['day_start']
-            row_dict['day_end'] = row['day_end']
-            dict_list.append(row_dict)
-    with open('weather_data_days.csv', 'w', encoding='utf-8', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(k for k, v in dict_list[0].items())
-        for d in dict_list:
-            print(d)
-            writer.writerow(v for k, v in d.items())
+        row_dict['text'] = row['text']
+        check_list = []
+        if cont:
+            for name in row_names:
+                row_dict[name] = row[name]
+                if int(row[name]) > 0:
+                    try:
+                        check_list.append(int(name))
+                    except ValueError:
+                        if re.match(r'^\d+-\d+', name):
+                            check_list.append(int(re.split('-', name)[0]))
+            if len(check_list) > 0 and not int(row['day_start']) in check_list:
+                print('--------')
+                print('day-start  :  '+  row['day_start'])
+                print('--------')
+                print('check_list')
+                print(check_list)
+                print('--------')
+                pp.pprint(row['text'])
+                print('day_start : ')
+                day_start = input()
+                print('day_end : ')
+                day_end = input()
+                print('--------')
+                print('Exit? (0 - exit, Eny other key to continue)')
+                try:
+                    cont = (bool(int(input())))
+                except ValueError:
+                    cont = True
+        else:
+            for name in row_names:
+                row_dict[name] = row[name]
+        row_dict['day_start'] = day_start
+        row_dict['day_end'] = day_end
+        dict_list.append(row_dict)
+with open('weather_data_days.csv', 'w', encoding='utf-8', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(k for k, v in dict_list[0].items())
+    for d in dict_list:
+        print(d)
+        writer.writerow(v for k, v in d.items())
+
